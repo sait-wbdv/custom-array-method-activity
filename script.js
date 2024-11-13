5f8be43831519704767fdf778e8472169ded0d57
Ashlyn Knox <ashlyn@lilyx.io>
2024-11-13 15:20:14 -0700

    script with comments

---
+++ script.js
@@ -0,0 +1,24 @@
+"use strict";
+
+// Use this array to test and practice with your code
+
+const movieArray = [
+  { title: "The Lord of the Rings", author: "J.R.R. Tolkien", year: 1954 },
+  { title: "Pride and Prejudice", author: "Jane Austen", year: 1813 },
+  { title: "1984", author: "George Orwell", year: 1949 },
+  {
+    title: "The Hitchhiker's Guide to the Galaxy",
+    author: "Douglas Adams",
+    year: 1979,
+  },
+];
+
+// 1. For Each: Exectutes a provided function once for each array element
+
+// 2. Filter: Returns a copy of an array with only the elements that pass a provided test
+
+// 3. Map: Returns a new array populated with results of the original array with a function executed on each element
+
+// 4. Reduce: Returns a single value after performing a provided "reducer" callback function ie. add all numbers in an array together
+
+// 5. Find: Returns the first element in an array that satisfies the provided test function
